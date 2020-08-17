34c7b00911d11d76711b4bc15089ed2e5b87a769
amie-affection <amie.affection@gmail.com>
2020-08-17 20:17:39 +0300

    added firebase

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import {} from "react-native";
 
+import { Provider } from "react-redux";
+
 import { NavigationContainer } from "@react-navigation/native";
 
 import { useRoute } from "./router";
+import { store } from "./redux/store";
 
 export default function App() {
   const routing = useRoute(true);
 
-  return <NavigationContainer>{routing}</NavigationContainer>;
+  return (
+    <Provider store={store}>
+      <NavigationContainer> {routing}</NavigationContainer>
+    </Provider>
+  );
 }
